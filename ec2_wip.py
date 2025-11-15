f170f3d9943090559dc0ce2684a1e74ed2ad0dd4
SJBSR <sjb.sr@outlook.com>
2025-11-15 17:23:17 -0500

    Start Ec2

--- ec2_wip.py
+++ ec2_wip.py
@@ -3,22 +3,12 @@ import boto3
 def client():
     return boto3.client('ec2')
 
-def describe_instances():
+def start_instances():
     client = boto3.client('ec2')
 
-response = client.describe_instances(
+response = client.start_instances(
     InstanceIds=[
         'string'
     ],
     DryRun=True|False,
-    Filters=[
-        {
-            'Name': 'string',
-            'Values': [
-                'string',
-            ]
-        },
-    ]   
 )
-NextToken='string',
-MaxResults=123
